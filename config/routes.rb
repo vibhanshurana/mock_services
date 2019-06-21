63340aada7714fbce52a1e24f33896c7cd487258
vibhanshu-rana <vibhanshu.rana@mintifi.com>
2019-06-21 17:19:57 +0530

    re-route APIs

--- config/routes.rb
+++ config/routes.rb
@@ -4,29 +4,23 @@ Rails.application.routes.draw do
 
   resources :mock_services
 
-  namespace :api do
-    namespace :v1 do
-      resources :finbits do
-        collection do
-          post 'login'
-          post 'uploadStatement'
-          get 'summary/:account_uid' => 'finbits#summary'
-        end
-      end
-    end
-  end
+  #APIs for Finbit
+  post 'bank-auth/api/v1/login' => 'api/v1/finbits#login'
+  post 'bank-connect/api/v1/uploadStatement' => 'api/v1/finbits#uploadStatement'
+  get 'bank-account/api/v1/summary/:account_uid' => 'api/v1/finbits#summary'
+  get 'bank-account/api/v1/peakUtilizationPerMonth/:account_uid' => 'api/v1/finbits#peakUtilizationPerMonth'
+  get 'bank-account/api/v1/topFiveCreditAndDebitInstances/:account_uid' => 'api/v1/finbits#topFiveCreditAndDebitInstances'
+  get 'bank-account/api/v1/loanReceivedInstances/:account_uid' => 'api/v1/finbits#loanReceivedInstances'
+  get 'bank-account/api/v1/penaltyInstances/:account_uid' => 'api/v1/finbits#penaltyInstances'
+  get 'bank-account/api/v1/internalTransactionsInstances/:account_uid' => 'api/v1/finbits#internalTransactionsInstances'
+  get 'bank-account/api/v1/monthwiseSummary/:account_uid' => 'api/v1/finbits#monthwiseSummary'
 
-  namespace :api do
-    namespace :v1 do
-      resources :karzas do
-        collection do
-          post 'gst-verification' => 'karzas#gst_verification'
-          post 'cinlookup'
-          post 'corp/profile' => 'karzas#corp_profile'
-          post 'ocr/kyc' => 'karzas#ocr_kyc'
-          post 'pan'
-        end
-      end
-    end
-  end
+  #APIs for Karza
+  post 'api/v3/karzas/gst-verification' => 'api/v1/karzas#gst_verification'
+  post 'api/v2/karzas/cinlookup' => 'api/v1/karzas#cinlookup'
+  post 'api/v1/karzas/corp/profile' => 'api/v1/karzas#corp_profile'
+  post 'api/v2/karzas/ocr/kyc' => 'api/v1/karzas#ocr_kyc'
+  post 'api/v2/karzas/pan' => 'api/v1/karzas#pan'
+  post 'api/v2/karzas/pan' => 'api/v1/karzas#pan'
+
 end
